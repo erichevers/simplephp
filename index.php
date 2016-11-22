f667d2146febc5d256065b0d49a7bff12df2322e
erichevers <erichevers@gmail.com>
2016-11-22 21:43:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 
 echo "<h1>Welcome to the best application in the world.";
 echo "<br><h1>Hello, world</h1>";
-echo "<br><h1>Hello, world</h1>";echo "<br><h1>Hello, world</h1>";echo "<br><h1>Hello, world</h1>";
+echo "<br><h1>Hello, world again</h1>";
+echo "<br><h1>Hello, world</h1>";
+echo "<br><h1>Hello, world</h1>";
 
