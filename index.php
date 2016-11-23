afa8d7ca2e063a1365bda86c18f61eefad761ad3
erichevers <erichevers@gmail.com>
2016-11-23 13:14:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ echo "<h1>Welcome to the best application in the world.";
 echo "<br><h1>Hello, world</h1>";
 echo "<br><h1>Hello, world again</h1>";
 echo "<br><h1>Hello, world and again</h1>";
-echo "<br><h1>Hello, world</h1>";
+echo "<br><h1>Hello, world change 4 for build 4</h1>";
 
