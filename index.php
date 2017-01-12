448bc40c67391a37400ad32cf6ac0d7d8d58bb5a
erichevers <erichevers@gmail.com>
2017-01-12 15:00:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,4 @@ echo "<br><h1>Hello, world and again</h1>";
 echo "<br><h1>Hello, world change 4 for build 4</h1>";
 echo "<br><h1>Hello, world change 5 for new project</h1>";
 echo "<br><h1>Hello, world - build 7</h1>";
-
+echo "<br><h1>Hello, world - build 8 mooi he</h1>";
